224721b56e9240b10af07b19573bb5873bee765c
Marianna <mckd2018@gmail.com>
2019-05-14 19:40:02 -0700

    add getting the price for each phone number for the call routing project

--- Lessons/source/scenario1.py
+++ Lessons/source/scenario1.py
@@ -1,8 +1,6 @@
-"""
-Scenario 1: One-time route cost check
-
+'''Scenario 1: One-time route cost check
 You have a carrier route list with 100,000 (100K) entries (in arbitrary order)
-and a single phone number. How quickly can you find the cost of calling this number?"""
+and a single phone number. How quickly can you find the cost of calling this number?'''
 # Check the book for number and get the price
 # Open the file put file info into a list
 # Loop through it
@@ -16,16 +14,17 @@ from hashtable import HashTable
 
 def load_data():
     """
-    Returns a list of phone numbers and prices from a file.
+    Returns a list of phone prefixes and prices from a file.
     """
     route_costs = []
     with open('../../project/data/route-costs-4.txt', 'r') as f:
         for line in f:
             prefix, price = line.split(',')
-            route_costs.append((prefix, price))
+            pprice = price.replace("\n", "")
+            route_costs.append((prefix, pprice))
     return route_costs
 
-def init_hashtable(route_costs, phone_num):
+def init_hashtable(route_costs):
     """
     Add phone number (key) and price (value) in the hashtable and return
     hashtable items to proven the data is inserted correctly.
@@ -34,30 +33,68 @@ def init_hashtable(route_costs, phone_num):
     num_buckets = len(route_costs)
     ht = HashTable(num_buckets)
     for prefix, price in route_costs: # number gives up +
-        print("Phone number and price:", prefix, price)
+        # print("Phone number and price:", prefix, price)
         ht.set(prefix, price)
-    print("Phone Number:", phone_num)
-    # Check if have a prefix that matches the start of a phone_num
-    if ht.contains(phone_num):
-        return print("Route Cost for {}: ${}".format(phone_num, ht.get(phone_num)))
-    else:
-        raise ValueError("Phone number not found.")
 
-def is_prefix_match_and_get_price(phone_num):
+    # Check if have a prefix that matches the start of a phone_num
+
+    return ht
+
+
+
+def is_prefix_match_and_get_price(ht, phone_num):
     """
     Return True if the prefix we have on record is the start of a phone we give
     as input otherwise return False
     """
-    # you've got a phone do we have a prefix that matches it
-    # contains
-    # get the price
+    print("Phone Number:", phone_num)
+    # base case
+    if not phone_num:
+        return 0
 
-    if self.contains(phone_num):
-        self.get()
+    #check if the phone number is in the HashTable
+    if ht.contains(phone_num):
+        # if yes, return the price
+        return ht.get(phone_num)
+    #if not, pop off the last digit of the phone number
+    else:
+        phone_num = phone_num[:-1]
+        return is_prefix_match_and_get_price(ht, phone_num)
+
+def get_prices(phone_numbers, is_prefix_match_and_get_price, ht):
+    #create prices list
+    price_list = []
+
+    #loop through the phone numbers
+    for number in phone_numbers:
+        #pass one phone number into the prefix match function
+        # append price to the list
+        price = is_prefix_match_and_get_price(ht, number)
+        price_list.append((number, price))
+
+    return price_list
+
+def load_phone_nums():
+    """
+    Returns a list of phone numbers.
+    """
+    phone_numbers = []
+
+    with open('../../project/data/phone-numbers-3.txt', 'r') as f:
+        for line in f:
+            # print(line)
+            individual_phone_num = line.replace("\n", "")
+            phone_numbers.append(individual_phone_num)
+
+    return phone_numbers
 
 
 if __name__ == '__main__':
     route_costs = load_data()
-    print("Phone Numbers and Prices:", route_costs)
-    print("***")
-    print(init_hashtable(route_costs, '+15124156620'))
+    # print("Phone Numbers and Prices:", route_costs)
+    # print("***")
+    ht = init_hashtable(route_costs)
+    phone_numbers = load_phone_nums()
+    print(get_prices(phone_numbers, is_prefix_match_and_get_price, ht))
+    # print(is_prefix_match_and_get_price(ht, phone_numbers))
+    # print(load_phone_nums())

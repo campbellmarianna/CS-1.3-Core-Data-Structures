4b4da9e2c59d8aa496c115612cb38492e0324560
Marianna <mckd2018@gmail.com>
2019-05-16 12:57:26 -0700

    started to optimize find_index_all

--- Lessons/source/strings.py
+++ Lessons/source/strings.py
@@ -1,10 +1,14 @@
 #!python
 
 def contains(text, pattern):
-    """Return a boolean indicating whether pattern occurs in text."""
+    """Return a boolean indicating whether pattern occurs in text.Best and worst
+     case running time: 0(t * p) for t characters in the text and p characters
+     in the pattern. We always have to loop through each character in the pattern
+      if it is found in the text
+      Space: o(1)"""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    result = find_index(text, pattern)
+    result = find_index(text, pattern) # (t * p)
     # if the pattern is not found in
     if result is None:
         return False
@@ -34,14 +38,17 @@ def contains(text, pattern):
 
 def find_index(text, pattern, start_index=0):
     """Return the starting index of the first occurrence of pattern in text,
-    or None if not found."""
+    or None if not found. Best and worst case running time: 0(t * p) for t
+    characters in the text and p characters in the pattern. We always have to loop
+    through each character in the pattern if it is found in the text
+    Space: 0(1)"""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     if pattern == '':
         return 0
     # Before we check the text and pattern lets make sure the pattern occurs in the text
-    for t_index, t_char in enumerate(text):
-        for p_index, p_char in enumerate(pattern):
+    for t_index in range(start_index, len(text)): # 0(t)
+        for p_index, p_char in enumerate(pattern): # Always p iterations because no early exit
             # check if more characters in the text match characters in the pattern
             text_char = text[t_index + p_index]
             # check if letters are the same
@@ -60,37 +67,60 @@ def find_index(text, pattern, start_index=0):
 
 def find_all_indexes(text, pattern):
     """Return a list of starting indexes of all occurrences of pattern in text,
-    or an empty list if not found."""
+    or an empty list if not found. Best and worst case running time: 0(t * p) for t
+    characters in the text and p characters in the pattern. We always have to loop
+    through each character in the pattern if it is found in the text
+    Space: O(t)"""
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # def find_all_index(text, pattern):
+    # match = []
+    # if pattern == '':
+    #     for index in range(len(text)):
+    #         match.append(index)
+    #     return match
+    # for t_index, t_char in enumerate(text):
+    #     for p_index, p_char in enumerate(pattern): # Always p iterations because no early exit
+    #         # check if more characters in the text match characters in the
+    #         # pattern
+    #         # check if it is a valid index of the text
+    #         if t_index + p_index > (len(text)-1):
+    #             break # not a valid index
+    #         text_char = text[t_index + p_index]
+    #         # check if letters are the same
+    #         if text_char == p_char:
+    #             # check if the letters are the same and we've reached the last
+    #             # index of the pattern
+    #             if p_index == (len(pattern) - 1):
+    #             # append the position of the charc where the pattern and text
+    #                 # first matched
+    #                 match.append(t_index)
+    #                 # append the text index minus the pattern index
+    #             continue
+    #         # they are not the same
+    #         else:
+    #             break
+    # # all characters in the pattern matched in the text
+    # return match
+
     match = []
     if pattern == '':
         for index in range(len(text)):
             match.append(index)
         return match
-    for t_index, t_char in enumerate(text):
-        for p_index, p_char in enumerate(pattern):
-            # check if more characters in the text match characters in the
-            # pattern
-            # check if it is a valid index of the text
-            if t_index + p_index > (len(text)-1):
-                break # not a valid index
-            text_char = text[t_index + p_index]
-            # check if letters are the same
-            if text_char == p_char:
-                # check if the letters are the same and we've reached the last
-                # index of the pattern
-                if p_index == (len(pattern) - 1):
-                # append the position of the charc where the pattern and text
-                    # first matched
-                    match.append(t_index)
-                    # append the text index minus the pattern index
-                continue
-            # they are not the same
-            else:
-                break
-    # all characters in the pattern matched in the text
+    result = 0
+
+    while result is not None and result < len(text):
+        if result == 0:
+            # call find index
+            result = find_index(text, pattern, result)
+            # append result to list
+            match.append(result)
+        else
+            # call find index
+            result = find_index(text, pattern, result + 1)
+            # append result to list
+            match.append(result)
+    # return list
     return match
 
 # if this pattern is contained in the text
@@ -130,4 +160,4 @@ def main():
 if __name__ == '__main__':
     # main()
     # print(find_all_indexes('aaaaa', 'aa'))
-    print(contains('abcabc', 'abcb'))
+    print(find_all_indexes('abc', 'a'))
